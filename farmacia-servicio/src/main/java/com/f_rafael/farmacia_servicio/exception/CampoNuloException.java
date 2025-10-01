d4b7f9817835dfdc0a4dbc7ff0c0cc7a68755b38
Rafael305 <fraq86@gmail.com>
2025-10-01 19:41:55 -0300

    excepciones personalizadas

---
+++ farmacia-servicio/src/main/java/com/f_rafael/farmacia_servicio/exception/CampoNuloException.java
@@ -0,0 +1,8 @@
+package com.f_rafael.farmacia_servicio.exception;
+
+public class CampoNuloException extends RuntimeException{
+
+    public CampoNuloException(String mensaje){
+        super(mensaje);
+    }
+}
---
+++ farmacia-servicio/src/main/java/com/f_rafael/farmacia_servicio/exception/EntidadNoEncontradaException.java
@@ -0,0 +1,8 @@
+package com.f_rafael.farmacia_servicio.exception;
+
+public class EntidadNoEncontradaException extends RuntimeException{
+
+    public EntidadNoEncontradaException(String mensaje){
+        super(mensaje);
+    }
+}
