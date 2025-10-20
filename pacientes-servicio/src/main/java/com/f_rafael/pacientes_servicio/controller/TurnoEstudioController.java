635298c86229aae9b488b5169f358c6894debd5d
Rafael305 <fraq86@gmail.com>
2025-10-20 15:13:07 -0300

    controladores patchmapping de turnoestudio

--- pacientes-servicio/src/main/java/com/f_rafael/pacientes_servicio/controller/TurnoEstudioController.java
+++ pacientes-servicio/src/main/java/com/f_rafael/pacientes_servicio/controller/TurnoEstudioController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
@@ -63,5 +64,49 @@ public class TurnoEstudioController {
     public ResponseEntity<String> borrarPorId(@PathVariable Long id){
         service.borrarPorId(id);
         return new ResponseEntity<>("Entidad borrada correctamente",HttpStatusCode.valueOf(204));
+    }
+
+    @PatchMapping
+    public ResponseEntity<TurnoEstudioDto> actualziarPaciente(@RequestParam Long id,
+                                                              @RequestParam("id-o-dni-paciente") Long idODniPaciente,
+                                                              @RequestParam String opcion){
+        return ResponseEntity.ok(service.actualizarPaciente(id,idODniPaciente,opcion));
+    }
+
+    @PatchMapping
+    public ResponseEntity<TurnoEstudioDto> actualizarFechaSolicitud(@RequestParam Long id,
+                                                                    @RequestParam("fecha-solicitud") LocalDate fechaSolicitud){
+        return ResponseEntity.ok(service.actualizarFechaSolicitud(id,fechaSolicitud));
+    }
+
+    @PatchMapping
+    public ResponseEntity<TurnoEstudioDto> actualizarFechaTurno(@RequestParam Long id,
+                                                                @RequestParam LocalDate fechaTurno){
+        return ResponseEntity.ok(service.actualizarFechaTurno(id,fechaTurno));
+    }
+
+    @PatchMapping
+    public ResponseEntity<TurnoEstudioDto> actualizarHorario(@RequestParam Long id,
+                                                             @RequestParam LocalTime inicio,
+                                                             @RequestParam LocalTime fin){
+        return ResponseEntity.ok(service.actualizarHorario(id,inicio,fin));
+    }
+
+    @PatchMapping
+    public ResponseEntity<TurnoEstudioDto> actualizarEstado(@RequestParam Long id,
+                                                            @RequestParam String estado){
+        return ResponseEntity.ok(service.actualizarEstado(id,estado));
+    }
+
+    @PatchMapping
+    public ResponseEntity<TurnoEstudioDto> actualizarCobertura(@RequestParam Long id,
+                                                               @RequestParam String cobertura){
+        return ResponseEntity.ok(service.actualizarCobertura(id,cobertura));
+    }
+
+    @PatchMapping
+    public ResponseEntity<TurnoEstudioDto> actualizarEstudio(@RequestParam Long id,
+                                                             @RequestParam("estudio-id") Long estudioId){
+        return ResponseEntity.ok(service.actualizarEstudio(id,estudioId));
     }
 }
