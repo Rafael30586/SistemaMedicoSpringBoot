9619f4462021ed2a132feac8c2d0b0f8598676d2
Rafael305 <fraq86@gmail.com>
2025-11-04 09:34:10 -0300

    metodos para actualizacion de CirugiaPaciente

--- hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/service/CirugiaPacienteService.java
+++ hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/service/CirugiaPacienteService.java
@@ -8,6 +8,7 @@ import com.f_rafael.hospital_servicio.exception.DatoIncorrectoException;
 import com.f_rafael.hospital_servicio.exception.EntidadNoEncontradaException;
 import com.f_rafael.hospital_servicio.mapper.CirugiaPacienteMapper;
 import com.f_rafael.hospital_servicio.model.CirugiaPaciente;
+import com.f_rafael.hospital_servicio.model.TratamientoQuirurgico;
 import com.f_rafael.hospital_servicio.repository.ICirugiaPacienteRepository;
 import com.f_rafael.hospital_servicio.repository.IPacienteClient;
 import com.f_rafael.hospital_servicio.utils.Verificador;
@@ -15,6 +16,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -95,5 +97,59 @@ public class CirugiaPacienteService implements ICirugiaPacienteService{
     @Override
     public List<CirugiaPacienteDto> buscarPorPeriodo(LocalDate desde, LocalDate hasta) {
         return mapper.obtenerListaDto(repository.buscarPorPeriodo(desde,hasta));
+    }
+
+    @Override
+    public CirugiaPacienteDto modificarPaciente(Long id, Long idODni, String opcion) {
+        CirugiaPaciente tratamientoParaActualizar = repository.findById(id).orElseThrow(()-> new EntidadNoEncontradaException("Tratamiento no encontrado"));
+
+        if(!verificador.esIdODni(opcion)){
+            throw new DatoIncorrectoException("Las opciones disponibles son id y dni");
+        }
+
+        if(opcion.equals("id")){
+            tratamientoParaActualizar.setPacienteId(idODni);
+        }
+
+        if(opcion.equals("dni")){
+            tratamientoParaActualizar.setPacienteId(pacienteClient.buscarPorDni(idODni).getId());
+        }
+
+        return this.actualizar(tratamientoParaActualizar);
+    }
+
+    @Override
+    public CirugiaPacienteDto modificarCirugia(Long id, Long cirugiaId) {
+        CirugiaPaciente tratamientoParaEditar = repository.findById(id).orElseThrow(()-> new EntidadNoEncontradaException("Tratamiento no encontrado"));
+        TratamientoQuirurgico cirugiaParaAsignar = new TratamientoQuirurgico();
+        cirugiaParaAsignar.setId(cirugiaId);
+
+        tratamientoParaEditar.setCirugia(cirugiaParaAsignar);
+
+        return this.actualizar(tratamientoParaEditar);
+    }
+
+    @Override
+    public CirugiaPacienteDto modificarFecha(Long id, LocalDate fecha) {
+        CirugiaPaciente tratamientoParaActualizar = repository.findById(id).orElseThrow(()-> new EntidadNoEncontradaException("Tratamiento no encontrado"));
+        tratamientoParaActualizar.setFecha(fecha);
+
+        return this.actualizar(tratamientoParaActualizar);
+    }
+
+    @Override
+    public CirugiaPacienteDto modificarHoraInicio(Long id, LocalTime inicio) {
+        CirugiaPaciente tratamientoParaActualizar = repository.findById(id).orElseThrow(()-> new EntidadNoEncontradaException("Tratamiento no encontrado"));
+        tratamientoParaActualizar.setInicio(inicio);
+
+        return this.actualizar(tratamientoParaActualizar);
+    }
+
+    @Override
+    public CirugiaPacienteDto modificarHoraFinal(Long id, LocalTime fin) {
+        CirugiaPaciente tratamientoParaActualizar = repository.findById(id).orElseThrow(()-> new EntidadNoEncontradaException("Tratamiento no encontrado"));
+        tratamientoParaActualizar.setFin(fin);
+        
+        return this.actualizar(tratamientoParaActualizar);
     }
 }
--- hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/service/ICirugiaPacienteService.java
+++ hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/service/ICirugiaPacienteService.java
@@ -5,6 +5,7 @@ import com.f_rafael.hospital_servicio.model.CirugiaPaciente;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 
@@ -18,4 +19,10 @@ public interface ICirugiaPacienteService {
     public List<CirugiaPacienteDto> buscarPorPaciente(Long idODni, String opcion);
     public List<CirugiaPacienteDto> buscarPorCirugia(String cirugia);
     public List<CirugiaPacienteDto> buscarPorPeriodo(LocalDate desde, LocalDate hasta);
+    public CirugiaPacienteDto modificarPaciente(Long id, Long idODni, String opcion);
+    public CirugiaPacienteDto modificarCirugia(Long id, Long cirugiaId);
+    public CirugiaPacienteDto modificarFecha(Long id, LocalDate fecha);
+    public CirugiaPacienteDto modificarHoraInicio(Long id, LocalTime inicio);
+    public CirugiaPacienteDto modificarHoraFinal(Long id, LocalTime fin);
+
 }
