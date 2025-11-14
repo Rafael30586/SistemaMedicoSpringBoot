0df60ef4fbdec71e1a7d73a1ea4dee622f43e7fe
Rafael305 <fraq86@gmail.com>
2025-11-14 09:11:08 -0300

    controladores para signo

---
+++ hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/controller/SignoController.java
@@ -0,0 +1,90 @@
+package com.f_rafael.hospital_servicio.controller;
+
+import com.f_rafael.hospital_servicio.dto.SignoDto;
+import com.f_rafael.hospital_servicio.model.Signo;
+import com.f_rafael.hospital_servicio.service.ISignoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/signos")
+public class SignoController {
+
+    private ISignoService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SignoDto> buscarPorId(@PathVariable Long id){
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SignoDto>> buscarTodos(){
+        return ResponseEntity.ok(service. buscarTodos());
+    }
+
+    @GetMapping
+    public ResponseEntity<SignoDto> buscarPorNombre(@RequestParam String nombre){
+        return ResponseEntity.ok(service.buscarPorNombre(nombre));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SignoDto>> buscarPorUnidad(@RequestParam String unidad){
+        return ResponseEntity.ok(service.buscarPorUnidad(unidad));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SignoDto>> buscarPorDescripcion(@RequestParam String secuencia){
+        return ResponseEntity.ok(service.buscarPorDescripcion(secuencia));
+    }
+
+    @PostMapping
+    public ResponseEntity<SignoDto> guardar(@RequestBody Signo signo){
+        return new ResponseEntity<>(service.guardar(signo), HttpStatusCode.valueOf(201));
+    }
+
+    @PutMapping
+    public ResponseEntity<SignoDto> actualizar(@RequestBody Signo signo){
+        return ResponseEntity.ok(service.actualizar(signo));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> borrarPorId(@PathVariable Long id){
+        service.borrarPorId(id);
+        return new ResponseEntity<>("El signo ha sido borrado", HttpStatusCode.valueOf(204));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<SignoDto> modificarNombre(@PathVariable Long id,
+                                                    @RequestParam String nombre){
+        return ResponseEntity.ok(service.modificarNombre(id, nombre));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<SignoDto> modificarValorMinimo(@PathVariable Long id,
+                                                         @RequestParam("valor-minimo") Double valorMinimo){
+        return ResponseEntity.ok(service.modificarValorMinimo(id, valorMinimo));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<SignoDto> modificarValorMaximo(@PathVariable Long id,
+                                                         @RequestParam("valor-maximo") Double valorMaximo){
+        return ResponseEntity.ok(service.modificarValorMaximo(id, valorMaximo));
+    }
+
+    @PatchMapping("/id")
+    public ResponseEntity<SignoDto> modificarUnidad(@PathVariable Long id,
+                                                    @RequestParam("unidad-id") Long unidadId){
+        return ResponseEntity.ok(service.modificarUnidad(id, unidadId));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<SignoDto> modificarDescripcion(@PathVariable Long id,
+                                                         @RequestParam String descripcion){
+        return ResponseEntity.ok(service.modificarDescripcion(id, descripcion));
+    }
+}
