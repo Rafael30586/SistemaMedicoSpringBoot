e57d03730ad24a876bf18bc322531f1d51ca3185
Rafael305 <fraq86@gmail.com>
2025-12-25 09:34:09 -0300

    interfaz HospitalClient

--- pacientes-servicio/src/main/java/com/f_rafael/pacientes_servicio/repository/HospitalClient.java
+++ pacientes-servicio/src/main/java/com/f_rafael/pacientes_servicio/repository/HospitalClient.java
@@ -1,13 +1,17 @@
 package com.f_rafael.pacientes_servicio.repository;
 
+import com.f_rafael.pacientes_servicio.dto.EstudioDto;
 import com.f_rafael.pacientes_servicio.dto.MedicoDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// @FeignClient(name = "HOSPITAL-SERVICIO")
-public interface IMedicoClient {
+@FeignClient(name = "HOSPITAL-SERVICIO")
+public interface HospitalClient {
+
+    @GetMapping("/estudios/{id}")
+    public EstudioDto obtenerEstudioPorId(@PathVariable Long id);
 
     @GetMapping("/empleados/{id}")
-    public MedicoDto obtenerInformacionMedico(@PathVariable Long id);
+    public MedicoDto obtenerMedicoPorId(@PathVariable Long id);
 }
--- pacientes-servicio/src/main/java/com/f_rafael/pacientes_servicio/repository/IEstudioClient.java
+++ pacientes-servicio/src/main/java/com/f_rafael/pacientes_servicio/repository/IEstudioClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "HOSPITAL-SERVICIO")
+// @FeignClient(name = "HOSPITAL-SERVICIO")
 public interface IEstudioClient {
 
     @GetMapping("/estudios/{id}")
