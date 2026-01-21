b41808a6adc6c5ee62a9f7d3904bb3fe6d9a68a0
Rafael305 <fraq86@gmail.com>
2026-01-21 10:46:51 -0300

    correccion de metodo en PacienteClient

--- hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/controller/FisioterapiaPacienteController.java
+++ hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/controller/FisioterapiaPacienteController.java
@@ -25,7 +25,7 @@ public class FisioterapiaPacienteController {
         return ResponseEntity.ok(service.buscarPorId(id));
     }
 
-    @GetMapping
+    @GetMapping // funciona
     public ResponseEntity<List<FisioterapiaPacienteDto>> buscarTodas(){
         return ResponseEntity.ok(service.buscarTodas());
     }
@@ -35,7 +35,7 @@ public class FisioterapiaPacienteController {
         return repository.findAll();
     }
 
-    @GetMapping("/paciente") // Este método no funciona cuando se utiliza la opción de dni porque and mal el findAll() del repositorio
+    @GetMapping("/paciente")
     public ResponseEntity<List<FisioterapiaPacienteDto>> buscarPorPaciente(@RequestParam("paciente-id-o-dni") Long pacienteIdODni,
                                                                      @RequestParam String opcion){
         return ResponseEntity.ok(service.buscarPorPaciente(pacienteIdODni, opcion));
--- hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/repository/IPacienteClient.java
+++ hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/repository/IPacienteClient.java
@@ -4,6 +4,7 @@ import com.f_rafael.hospital_servicio.dto.PacienteDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "PACIENTES-SERVICIO")
 public interface IPacienteClient {
@@ -11,6 +12,6 @@ public interface IPacienteClient {
     @GetMapping("/pacientes/{id}")
     public PacienteDto buscarPacientePorId(@PathVariable Long id);
 
-    @GetMapping("/pacientes/{dni}")
-    public PacienteDto buscarPacientePorDni(@PathVariable Long dni);
+    @GetMapping("/pacientes/dni")
+    public PacienteDto buscarPacientePorDni(@RequestParam Long dni);
 }
