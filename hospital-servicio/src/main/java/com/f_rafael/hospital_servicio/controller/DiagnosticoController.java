0b8b3048ebb8cdc36e9de8338cdad0a98ff22aaa
Rafael305 <fraq86@gmail.com>
2026-02-01 10:15:38 -0300

    comprobacion de funcionamiento de controladores de entidad diagnostico

--- hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/controller/DiagnosticoController.java
+++ hospital-servicio/src/main/java/com/f_rafael/hospital_servicio/controller/DiagnosticoController.java
@@ -17,56 +17,56 @@ public class DiagnosticoController {
 
     private IDiagnosticoService service;
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") // funciona
     public ResponseEntity<DiagnosticoDto> buscarPorId(@PathVariable Long id){
         return ResponseEntity.ok(service.buscarPorId(id));
     }
 
-    @GetMapping
+    @GetMapping // funciona
     public ResponseEntity<List<DiagnosticoDto>> buscarTodos(){
         return ResponseEntity.ok(service.buscarTodos());
     }
 
-    @PostMapping
+    @GetMapping("/nombre") // funciona
+    public ResponseEntity<DiagnosticoDto> buscarPorNombre(@RequestParam String nombre){
+        return ResponseEntity.ok(service.buscarPorNombre(nombre));
+    }
+
+    @PostMapping // funciona
     public ResponseEntity<DiagnosticoDto> guardar(@RequestBody Diagnostico diagnostico){
         return new ResponseEntity<>(service.guardar(diagnostico), HttpStatusCode.valueOf(201));
     }
 
-    @PutMapping
+    @PutMapping // funciona
     public ResponseEntity<DiagnosticoDto> actualizar(@RequestBody Diagnostico diagnostico){
         return ResponseEntity.ok(service.actualizar(diagnostico));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}") // funciona
     public ResponseEntity<String> borrarPorId(@PathVariable Long id){
         service.borrarPorId(id);
         return new ResponseEntity("El diagnóstico se ha borrado",HttpStatusCode.valueOf(204));
     }
 
-    @GetMapping("/nombre")
-    public ResponseEntity<DiagnosticoDto> buscarPorNombre(@RequestParam String nombre){
-        return ResponseEntity.ok(service.buscarPorNombre(nombre));
-    }
-
-    @PatchMapping("/{id}/agregar-sintoma")
+    @PatchMapping("/{id}/agregar-sintoma") // funciona
     public ResponseEntity<DiagnosticoDto> agregarSintoma(@PathVariable Long id,
                                                          @RequestParam("sintoma-id") Long sintomaId){
         return ResponseEntity.ok(service.agregarSintoma(id, sintomaId));
     }
 
-    @PatchMapping("/{id}/quitar-sintoma")
+    @PatchMapping("/{id}/quitar-sintoma") // funciona
     public ResponseEntity<DiagnosticoDto> quitarSintoma(@PathVariable Long id,
                                                         @RequestParam("sintoma-id") Long sintomaId){
         return ResponseEntity.ok(service.quitarSintoma(id, sintomaId));
     }
 
-    @PatchMapping("/{id}/agregar-signo")
+    @PatchMapping("/{id}/agregar-signo") // funciona
     public ResponseEntity<DiagnosticoDto> agregarSigno(@PathVariable Long id,
                                                        @RequestParam("signo-id") Long signoId){
         return ResponseEntity.ok(service.agregarSigno(id, signoId));
     }
 
-    @PatchMapping("/{id}/quitar-signo")
+    @PatchMapping("/{id}/quitar-signo") // funciona
     public ResponseEntity<DiagnosticoDto> quitarSigno(@PathVariable Long id,
                                                       @RequestParam("signo-id") Long signoId){
         return ResponseEntity.ok(service.quitarSigno(id, signoId));
